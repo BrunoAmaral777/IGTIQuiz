84e7699f776f5b8a27476992198de54ef6295088
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-10-08 16:24:33 -0300

    Salvando no shared preferences primeiro acesso

--- QuizIGTI/app/src/main/java/com/example/quizigti/SplashScreenActivity.java
+++ QuizIGTI/app/src/main/java/com/example/quizigti/SplashScreenActivity.java
@@ -3,6 +3,7 @@ package com.example.quizigti;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -17,11 +18,24 @@ public class SplashScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
-            }
-        },3000);
+        getSplash();
+    }
+
+    private void getSplash() {
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        final SharedPreferences.Editor editor = preferences.edit();
+        Boolean firstLogin = preferences.getBoolean("FIRST", true);
+        if (firstLogin) {
+            editor.putBoolean("FIRST", false);
+            editor.commit();
+            new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                }
+            }, 1000);
+        }else {
+            startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+        }
     }
 }
