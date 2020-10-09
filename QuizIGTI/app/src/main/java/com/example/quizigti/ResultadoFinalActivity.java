906d57fdb0fbb1ab94cd3162e973e7f306b53bce
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-10-09 10:25:52 -0300

    Criando lógica para mostrar mensagem de acerto e erro

--- QuizIGTI/app/src/main/java/com/example/quizigti/ResultadoFinalActivity.java
+++ QuizIGTI/app/src/main/java/com/example/quizigti/ResultadoFinalActivity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class ResultadoFinalActivity extends AppCompatActivity {
-
+Button btnReturn = findViewById(R.id.btnReturn);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,5 +27,13 @@ public class ResultadoFinalActivity extends AppCompatActivity {
         }
         progressBar.setProgress(percent);
         txvProgres.setText(percent + "%");
+
+        btnReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ResultadoFinalActivity.this, MainActivity.class));
+                finish();
+            }
+        });
     }
 }
