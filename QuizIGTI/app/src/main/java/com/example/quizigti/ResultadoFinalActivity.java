a07d4504ee58cb628041d1046e27d9a4f9210d45
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-10-09 08:57:53 -0300

    criação do estilo para o progress bar

--- QuizIGTI/app/src/main/java/com/example/quizigti/ResultadoFinalActivity.java
+++ QuizIGTI/app/src/main/java/com/example/quizigti/ResultadoFinalActivity.java
@@ -3,6 +3,8 @@ package com.example.quizigti;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class ResultadoFinalActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class ResultadoFinalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado_final);
+        ProgressBar progressBar = findViewById(R.id.progress_bar);
+        TextView txvProgres = findViewById(R.id.text_view_progress);
+
+        progressBar.setProgress(60);
+        txvProgres.setText(60 +"%");
     }
 }
